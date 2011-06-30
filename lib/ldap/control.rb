57ad73b0c9c624a16e9e6d9dd8207743701f12d4
Michael Granger <ged@FaerieMUD.org>
2011-06-30 15:53:00 -0700

    Added missing implementation of LDAP::Control

--- lib/ldap/control.rb
+++ lib/ldap/control.rb
@@ -13,7 +13,7 @@ module LDAP
     # Take +vals+, produce an Array of values in ASN.1 format and then
     # convert the Array to DER.
     #
-    def Control.encode( *vals )
+    def self::encode( *vals )
       encoded_vals = []
 
       vals.each do |val|
@@ -32,16 +32,59 @@ module LDAP
     end
 
 
-    # Take an Array of ASN.1 data and return an Array of decoded values.
-    #
+    # Create a new LDAP::Control. +oid+ is the OID of the control, +value+ is the
+    # value to be assigned to the control, and +criticality+ is the criticality
+    # of the control, which should be *true* or *false*.
+    def initialize( oid=nil, value=nil, criticality=false )
+      @oid      = oid
+      @value    = value
+      @critical = criticality
+    end
+
+
+    # Copy constructor -- dup the Control's instance variables.
+    def initialize_copy( original )
+      super
+
+      @oid      = original.oid.dup if original.oid
+      @value    = original.value.dup if original.value
+      @critical = original.critical?
+    end
+
+
+    ######
+    public
+    ######
+
+    # The OID of the control in numeric format
+    attr_accessor :oid
+
+    # The ASN1-encoded value of the control
+    attr_accessor :value
+
+    # The criticality of the control
+    attr_accessor :critical
+    alias_method :critical?, :critical
+    alias_method :iscritical, :critical
+    alias_method :iscritical=, :critical=
+
+
+    # Decode the Control's ASN1-encoded value and return its contents as an Array.
     def decode
-      values = []
-
-      OpenSSL::ASN1::decode( self.value ).value.each do |val|
-	      values << val.value
+      return OpenSSL::ASN1::decode( self.value ).value.inject( [] ) do |ary, val|
+	      ary << val.value
       end
+    end
 
-      values
+
+    # Produce a concise representation of the control.
+    def inspect
+      "#<%s oid=%p value=%p iscritical=%p>" % [
+        self.class.name,
+        self.oid,
+        self.value,
+        self.critical?
+        ]
     end
 
   end
